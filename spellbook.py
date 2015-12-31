2812ca294afdbfe493632ece6828b75ce1a6b298
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2015-12-31 23:10:51 +0100

    FEAT add command

--- spellbook.py
+++ spellbook.py
@@ -1,21 +1,52 @@
 import argparse
+import json
 import os
 
-import sys
-
 MAIN_DIRECTORY = os.path.join(os.path.expanduser('~'), '.spellbook')
+if not os.path.exists(MAIN_DIRECTORY):
+    os.makedirs(MAIN_DIRECTORY)
 CONFIG_FILE = os.path.join(MAIN_DIRECTORY, 'config')
 DATABASE_FILE = os.path.join(MAIN_DIRECTORY, 'database')
-
-print("hejo")
 
 
 def command_search(args):
     print('command search')
 
 
+def collect_str(what):
+    result = input("provide %s>> " % what)
+    return result
+
+
+def save_spell(cmd, desc):
+    val = {
+        'cmd': cmd,
+        'desc': desc
+    }
+    with open(DATABASE_FILE, 'at') as f:
+        f.write(json.dumps(val))
+
+
 def command_add(args):
     print('command add')
+
+    data = args.data
+
+    if len(data) == 2:
+        cmd = data[0]
+        desc = data[1]
+    elif len(data) == 1:
+        cmd = data[0]
+        desc = collect_str('description')
+    elif len(data) == 0:
+        cmd = collect_str('command')
+        desc = collect_str('description')
+    else:
+        print("ERR: got too much arguments (2 is max, got: %d)" % len(data))
+        return
+
+    print("%s::%s" % (cmd, desc))
+    save_spell(cmd, desc)
 
 
 def prepare_argparse():
@@ -25,8 +56,9 @@ def prepare_argparse():
     parser_search = subparsers.add_parser('search')
     parser_search.set_defaults(func=command_search)
 
-    parser_add = subparsers.add_parser('add')
+    parser_add = subparsers.add_parser('add', aliases=['a'])
     parser_add.set_defaults(func=command_add)
+    parser_add.add_argument('data', default=None, nargs='*')
 
     return parser
 
@@ -34,7 +66,7 @@ def prepare_argparse():
 def main():
     parser = prepare_argparse()
     args = parser.parse_args()
-    print(args.command_name)
+    print(args)
     args.func(args)
 
 
