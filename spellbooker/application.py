fe44deaa2f5661054de33bc1cbccf6bec895ae09
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2016-02-02 13:40:31 +0100

    change version to 1.1.0

--- spellbooker/application.py
+++ spellbooker/application.py
@@ -7,7 +7,7 @@ import os
 
 from dropbox.files import WriteMode
 
-VERSION = '1.0.2'
+VERSION = '1.1.0'
 
 MAIN_DIRECTORY = os.path.join(os.path.expanduser('~'), '.spellbook')
 CONFIG_DIRECTORY = os.path.join(MAIN_DIRECTORY, 'config')
