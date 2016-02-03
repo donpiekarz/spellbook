7a0d0a57a284217950c86b5ed9933ef25f2feae7
Bartlomiej Piekarski <bartlomiej.piekarski@gmail.com>
2016-02-03 14:53:39 +0100

    version 1.1.4

--- spellbooker/application.py
+++ spellbooker/application.py
@@ -8,7 +8,7 @@ import json
 import os
 from builtins import *
 
-VERSION = '1.1.3'
+VERSION = '1.1.4'
 
 MAIN_DIRECTORY = os.path.join(os.path.expanduser('~'), '.spellbook')
 CONFIG_DIRECTORY = os.path.join(MAIN_DIRECTORY, 'config')
